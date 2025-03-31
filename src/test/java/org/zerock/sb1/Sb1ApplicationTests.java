7f298f3e80c6ed08739d8c2e086e948e4d2e3ee9
zerock <kangckck24@gmail.com>
2025-03-31 10:41:44 +0900

    abbb

--- src/test/java/org/zerock/sb1/Sb1ApplicationTests.java
+++ src/test/java/org/zerock/sb1/Sb1ApplicationTests.java
@@ -3,11 +3,17 @@ package org.zerock.sb1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootTest
+@Log4j2
 class Sb1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("---------------------------------");
+		log.info("---------------------------------");
+		log.info("---------------------------------");
 	}
 
 }
