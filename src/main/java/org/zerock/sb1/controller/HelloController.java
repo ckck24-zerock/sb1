106fdf9ea634cbdf944f481971972e5c084dfebc
zerock <kangckck24@gmail.com>
2025-03-31 10:30:23 +0900

    0331

---
+++ src/main/java/org/zerock/sb1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.zerock.sb1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+  
+  @GetMapping("hello")
+  public String[] hello(){
+    return new String[]{"AAA","BBB","CCC"};
+  }
+
+}
